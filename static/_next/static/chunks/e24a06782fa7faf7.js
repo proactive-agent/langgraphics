30422269f668ea40039652fc89d28ebe94a0be1f
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2026-02-10 13:22:38 +0400

    Delete the static website content

--- static/_next/static/chunks/e24a06782fa7faf7.js
+++
@@ -1,4 +0,0 @@
-;!function(){try { var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof global?global:"undefined"!=typeof window?window:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&((e._debugIds|| (e._debugIds={}))[n]="ce03bc95-4f13-50d7-cad0-af5d2644959b")}catch(e){}}();
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,674576,(e,t,r)=>{"use strict";Object.defineProperty(r,"__esModule",{value:!0}),Object.defineProperty(r,"warnOnce",{enumerable:!0,get:function(){return n}});let n=e=>{}}]);
-
-//# debugId=ce03bc95-4f13-50d7-cad0-af5d2644959b
