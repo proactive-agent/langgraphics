46cb7e55e1b8d453785ae3d1e12b87e71db3aec3
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2026-02-12 19:39:54 +0400

    Ignore the noise of the WS handshake failures

--- langgraphics/__init__.py
+++ langgraphics/__init__.py
@@ -1,3 +1,8 @@
+import logging
+
 from .watch import watch
+
+# ignore the expected noise of the websocket handshake failures
+logging.getLogger("websockets.server").addFilter(lambda _: False)
 
 __all__ = ["watch"]
