85780ce4b652913cd8b9a24fd8bb713945321a77
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2026-03-02 02:20:27 +0400

    Enhance Formatter class with metrics extraction and improved message handling

--- langgraphics/formatter.py
+++ langgraphics/formatter.py
@@ -1,15 +1,9 @@
 import json
+from collections import deque
 from typing import Any
 
 from langchain_core.messages import messages_to_dict
 from langchain_core.tracers.schemas import Run
-from langsmith.utils import (
-    get_message_generation_from_outputs,
-    get_llm_generation_from_outputs,
-    get_messages_from_inputs,
-    get_prompt_from_inputs,
-    _convert_message,
-)
 
 
 class Formatter:
@@ -25,7 +19,36 @@ class Formatter:
         return wrapper
 
     @staticmethod
+    def extract(data: dict, key: str):
+        def bfs():
+            queue = deque([data])
+            while queue:
+                current = queue.popleft()
+                if isinstance(current, dict):
+                    if key in current:
+                        yield current[key]
+                    queue.extend(current.values())
+                elif isinstance(current, (list, tuple)):
+                    queue.extend(current)
+
+        return next(bfs(), None)
+
+    @classmethod
+    def metrics(cls, run: Run) -> dict[str, Any]:
+        return {
+            "latency": (run.end_time - run.start_time).total_seconds(),
+            "tokens": cls.extract(run.outputs, "total_tokens"),
+            # TODO: get the cost from the model costs metadata - coming soon
+            # "cost": cls.extract(run.extra, "model"),  # model_name
+        }
+
+    @staticmethod
     def norm(msg: dict[str, Any]) -> dict[str, Any]:
+        if "lc" in msg:
+            msg = {
+                "type": msg["id"][-1].replace("Message", "").lower(),
+                "data": msg["kwargs"],
+            }
         data: dict[str, Any] = msg.get("data", {})
         if tool_calls := data.get("tool_calls", []):
             fmt_tool = lambda tc: f"{tc.get('name', '?')}({tc.get('args', {})})"
@@ -50,16 +73,18 @@ class Formatter:
                 messages = data["messages"][0]
             except KeyError:
                 return [data]
-            return list(map(cls.norm, get_messages_from_inputs({"messages": messages})))
+            return list(map(cls.norm, messages))
         elif run.run_type == "chain":
             messages = messages_to_dict(data.get("messages", []))
-            return list(map(cls.norm, get_messages_from_inputs({"messages": messages})))
+            return list(map(cls.norm, messages))
         elif run.run_type == "tool":
             return [{"role": "input", "content": str(data.get("input", ""))}]
         elif run.run_type == "retriever":
             return [{"role": "query", "content": str(data.get("query", ""))}]
         elif run.run_type == "llm":
-            return [{"role": "prompt", "content": get_prompt_from_inputs(data)}]
+            if "prompts" in data:
+                return [{"role": "prompt", "content": data["prompts"][0]}]
+            return [{"role": "prompt", "content": data["prompt"]}]
         return []
 
     @classmethod
@@ -69,12 +94,7 @@ class Formatter:
             return [{"role": "error", "content": str(run.error)}]
         data: dict[str, Any] = run.outputs or {}
         if run.run_type == "chat_model":
-            try:
-                message = run.outputs["generations"][0][0]["message"]
-                generation = _convert_message(message)
-            except IndexError:
-                generation = get_message_generation_from_outputs(data)
-            return [cls.norm(generation)]
+            return [cls.norm(cls.extract(data, "message"))]
         elif run.run_type == "chain":
             try:
                 messages = messages_to_dict(data["messages"].value)
@@ -82,9 +102,9 @@ class Formatter:
                 messages = messages_to_dict(data["messages"])
             except KeyError:
                 return [data]
-            return list(map(cls.norm, get_messages_from_inputs({"messages": messages})))
+            return list(map(cls.norm, messages))
         elif run.run_type == "llm":
-            return [{"role": "text", "content": get_llm_generation_from_outputs(data)}]
+            return [{"role": "text", "content": data["generations"][0]["text"]}]
         elif run.run_type == "tool":
             out = data.get("output", "")
             return [{"role": "output", "content": getattr(out, "content", str(out))}]
